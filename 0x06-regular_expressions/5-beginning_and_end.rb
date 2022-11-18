fc6b39ba836129ddb9fcf6b5f5dfdce80d70be09
arabiu033 <arabiu033@gmail.com>
2022-11-18 10:33:28 -0800

    5. Not quite HBTN yet

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
+puts ARGV[0].scan(/^h.n$/).join
